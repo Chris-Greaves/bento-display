fee61705c911dfaec42ff4b2452c28f6c43c86ee
Christopher Greaves <28542943+Chris-Greaves@users.noreply.github.com>
2026-01-10 02:11:12 +0000

    A simple first go at a bento style photo gallery

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import './layout.css';
+	import favicon from '$lib/assets/favicon.svg';
+
+	let { children } = $props();
+</script>
+
+<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<div class="min-h-screen flex flex-col items-center justify-start p-4 bg-gray-900 text-gray-100">
+	{@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+	import { lazyLoad } from '$lib/lazyload.js';
+
+	const cardClass = 'w-full h-auto min-h-96 object-scale-down rounded-lg shadow-md bg-gray-800';
+</script>
+
+<h1 class="p-8 text-center text-4xl">Congratulations Mr & Mrs Greaves!</h1>
+
+<div class="grid grid-cols-1 gap-4 md:hidden">
+	<img use:lazyLoad={"/images/photo1.JPG"} class={cardClass} />
+	<img use:lazyLoad={"/images/photo2.JPG"} class={cardClass} />
+	<img use:lazyLoad={"/images/photo3.JPG"} class={cardClass} />
+	<img use:lazyLoad={"/images/photo4.JPG"} class={cardClass} />
+	<img use:lazyLoad={"/images/photo5.JPG"} class={cardClass} />
+</div>
+
+<div class="hidden grid-cols-3 gap-4 md:grid">
+	<div class="col-span-1">
+		<div class="grid grid-cols-1 gap-4">
+			<img use:lazyLoad={"/images/photo0.JPG"} alt="Wedding 1" class={cardClass} />
+			<img use:lazyLoad={"/images/photo3.JPG"} alt="Wedding 2" class={cardClass} />
+			<img use:lazyLoad={"/images/photo6.JPG"} alt="Wedding 3" class={cardClass} />
+			<img use:lazyLoad={"/images/photo9.JPG"} alt="Wedding 9" class={cardClass} />
+		</div>
+	</div>
+	<div class="col-span-1">
+		<div class="grid grid-cols-1 gap-4">
+			<img use:lazyLoad={"/images/photo1.JPG"} alt="Wedding 2" class={cardClass} />
+			<img use:lazyLoad={"/images/photo4.JPG"} alt="Wedding 3" class={cardClass} />
+			<img use:lazyLoad={"/images/photo7.JPG"} alt="Wedding 1" class={cardClass} />
+		</div>
+	</div>
+	<div class="col-span-1">
+		<div class="grid grid-cols-1 gap-4">
+			<img use:lazyLoad={"/images/photo2.JPG"} alt="Wedding 3" class={cardClass} />
+			<img use:lazyLoad={"/images/photo5.JPG"} alt="Wedding 1" class={cardClass} />
+			<img use:lazyLoad={"/images/photo8.JPG"} alt="Wedding 2" class={cardClass} />
+		</div>
+	</div>
+</div>
+
+<style>
+	img {
+		opacity: 0;
+		transition: all 1s ease;
+	}
+</style>
