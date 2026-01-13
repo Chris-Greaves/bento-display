1a5f38b37c427e2b0ecfb977270b5ca89df46191
Christopher Greaves <28542943+Chris-Greaves@users.noreply.github.com>
2026-01-13 02:51:10 +0000

    Update API to handle missing imageData file

--- src/routes/api/imageData/+server.ts
+++ src/routes/api/imageData/+server.ts
@@ -2,8 +2,14 @@ import { env } from '$env/dynamic/private';
 import fs from 'fs';
 import path from 'path';
 
-const imageData = fs.readFileSync(path.resolve(env.IMAGE_DIR, "imageData.json"));
-
-export function GET() {
-	return Response.json(JSON.parse(imageData.toString()));
+export async function GET() {
+	// Asynchronously read 'input1.txt'
+	fs.readFile(path.resolve(env.IMAGE_DIR, "imageData.json"), { encoding: 'utf8', flag: 'r' }, (err: any, data: any) => {
+		if (err) {
+			console.error('Error reading imageData.json:', err);
+			return Response.json({imageData: []});
+		} else {
+			return Response.json(JSON.parse(data.toString()));
+		}
+	});
 }
